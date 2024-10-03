5e400e4ac18bd9f43acbca322f35a2298a9101c1
k-group <89579850+k-group@users.noreply.github.com>
2024-10-03 17:24:54 +0900

    [add] a script to show cache dir of incomplete wall/word2vec/link_prediction process

---
+++ check_cache_dir.py
@@ -0,0 +1,38 @@
+
+from glob import glob
+import os
+from datetime import datetime
+
+'''
+cache directory にあるもののうち，処理が終わらずに Job が終了したものをリストアップするスクリプト
+'''
+
+
+for mrm in ['rdr', 'rc', 'sgprop']:
+  print(f"++ {mrm} ++")
+  target_dir = f'cache/{mrm}'
+  
+  finished_processes = set()
+  oldest_time = None
+  with open(f"{target_dir}/progress.txt") as fp:
+      for line in fp:
+          *_, path, state = line.split(" ")
+          path = path[:-1]
+          if oldest_time is None:
+              time_str = ' '.join(line.split(' ', 3)[:2])
+              oldest_time = datetime.fromisoformat(time_str)
+              print(f'[OLDEST LOG TIME]: {oldest_time}')
+          finished_processes.add(path)
+          # print(path)
+  
+  print(len(finished_processes))
+  
+  for name in ['walk', 'link_prediction_model', 'word2vec']:
+      for dir_path in sorted(glob(f'{target_dir}/{name}/*'), key=lambda f: os.stat(f).st_mtime, reverse=True):
+        if dir_path not in finished_processes:
+          if datetime.fromtimestamp(os.stat(dir_path).st_mtime) < oldest_time:
+              continue
+          print(f"\n== {dir_path} ==")
+          for child in glob(f"{dir_path}/**/*", recursive=True):
+              print(child)
+  
